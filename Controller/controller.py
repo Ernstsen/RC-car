d99482e66cfa4bdb24087fb541d825aac0f26a16
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2021-02-10 18:59:07 +0100

    moved server connectivity to own module

--- Controller/controller.py
+++ Controller/controller.py
@@ -1,18 +1,4 @@
-# Python program to implement client side of the control-flow
-
-def main():
-    import socket
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('127.0.0.1', 8080))
-    while True:
-        inp = input("Input:")
-        if inp == 'q':
-            break
-        client.send(inp.encode())
-        from_server = client.recv(4096).decode()
-        print(str(from_server))
-    client.close()
-
+from server_connector import *
 
 if __name__ == "__main__":
-    main()
+    run_test()
---
+++ Controller/server_connector.py
@@ -0,0 +1,61 @@
+# Python program to implement client side of the control-flow
+import socket
+
+
+def connect(address: str, port: int) -> socket:
+    """
+    Establishes a connection to a server
+
+    :param address: server address to connect to
+    :param port: port to be used
+    :return: socket with an active connection to the specified server
+    """
+    conn: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.connect((address, port))
+    return conn
+
+
+def send(conn: socket, msg: str, msg_length: int = 4096) -> bool:
+    """
+    Sends a message through the supplied connection, and returns whether a response is received
+    If a response is received it's printed
+
+    :param conn: connection to send the message through
+    :param msg: message to be sent
+    :param msg_length: length of the message to be sent. Default is 4096
+    :return: whether a response has been received and logged to terminal
+    """
+    conn.send(msg.encode())
+    from_server = conn.recv(msg_length).decode()
+    if not from_server:
+        return False
+    print(str(from_server))
+    return True
+
+
+def terminate(conn: socket) -> None:
+    """
+    terminates the connection of a socket
+
+    :param conn: connection to be terminated
+    """
+    conn.close()
+
+
+def run_test() -> None:
+    """
+    executes a test of the connector, connecting to a socket server at 127.0.0.1:8080,
+    then takes input in a while(True) loop, which can be broken with input='q'
+    each input different from "q" is sent to the server
+    """
+    client = connect("127.0.0.1", 8080)
+    while True:
+        inp = input("Input:")
+        if inp == 'q':
+            break
+        send(client, inp)
+    terminate(client)
+
+
+if __name__ == "__main__":
+    run_test()
