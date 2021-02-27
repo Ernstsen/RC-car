315fa995beb0929c5ef22cce71f2de6b1c973746
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2021-02-27 18:52:47 +0100

    introduced way of interfacing with RC vehicle

---
+++ Controller/vehicle_control/controller_simulator.py
@@ -0,0 +1,29 @@
+from .controller import Controller
+
+
+class ControllerSimulator(Controller):
+    """
+    Controller implementation with
+    """
+
+    def __init__(self):
+        self.drive: int = 0
+        self.gear: int = 1
+        self.throttle: int = 0
+        self.direction: int = 5
+
+    def set_drive(self, val: int) -> None:
+        if 0 <= val <= 1:
+            self.drive = val
+
+    def set_gear(self, val: int) -> None:
+        if 1 <= val <= 4:
+            self.gear = val
+
+    def set_throttle(self, val: int) -> None:
+        if 0 <= val <= 10:
+            self.throttle = val
+
+    def set_direction(self, val: int) -> None:
+        if 0 <= val <= 10:
+            self.direction = val
