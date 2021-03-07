341a77e0a375fe8d45439805098d6923b893fbc6
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2021-03-07 13:53:19 +0100

    application now talks to self-hosted server as standard

--- Controller/application.py
+++ Controller/application.py
@@ -1,4 +1,8 @@
-from Controller.communication.server_utilities import *
+from Controller.gui.application_gui import GUI
+from Controller.vehicle_control import VehicleController
 
 if __name__ == "__main__":
-    run_test()
+    controller: VehicleController = VehicleController("127.0.0.1", 8080)
+
+    gui = GUI(controller=controller)
+    gui.mainloop()
