b227ac4e40daeddbbaf302b085403d060925309c
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2021-03-08 09:56:58 +0100

    sketched vehicle state struct and expanded api

--- PropellerBoard/PROPELLER_VEHICLE.md
+++ PropellerBoard/PROPELLER_VEHICLE.md
@@ -6,3 +6,20 @@ a [Propeller Activity Board WX](https://www.parallax.com/product/propeller-activ
 The code is developed using
 the [SimpleIDE](https://learn.parallax.com/tutorials/language/propeller-c/propeller-c-set-simpleide), which is also used
 in deploying the software to the developer board.
+
+## Structure
+
+The propeller board is based on the ``Parallax P8X32A`` octa-core processor, enabling proper multi-thread applications.
+This project will utilize this, by using between 1 and 8 threads, each assigned to a task, as follows:
+
+1. *Main Thread* initializes the other threads, and listens for messages sent through serial connection
+2. *Currently Unused*
+3. *Currently Unused*
+4. *Currently Unused*
+5. *Currently Unused*
+6. *Currently Unused*
+7. *Currently Unused*
+8. *Currently Unused*
+
+Each non-main thread has behaviour defined in own file, in an attempt at improved readability. Main-thread behaviour is
+found in ``vehicle.c``
--- PropellerBoard/vehicle.c
+++ PropellerBoard/vehicle.c
@@ -1,31 +1,56 @@
-/*
-  Blank Simple Project.c
-  http://learn.parallax.com/propeller-c-tutorials 
-*/
-#include "simpletools.h"                      // Include simple tools
-#include "fdserial.h"                         // serial communication          
-  
-int main()                                    // Main function
+#include "simpletools.h"    // Include simple tools
+#include "fdserial.h"       // serial communication
+
+typedef struct {
+    int8_t lights;      //Whether lights are on - acts like boolean
+    int8_t drive;       //Low(0)/High(1) drive
+    int8_t gear;        //Between 1 and 4
+    int8_t throttle;    // between 0 and 10 - 0 being off
+    int8_t direction;   //between 0 and 10, 5 being centered
+} vehicle_state;
+
+int main()
 {
-  simpleterm_close();
-  
-  fdserial *ser = fdserial_open(31, 30, 0, 115200);
-  
-  int val;  
-   
-  char c;
- 
-  while(1)
-  {
-    c = fdserial_rxCheck(ser);
-    if(c != -1){
-      
-      switch(c) {
-        case 'D' ://Drive
-          val = fdserial_rxCheck(ser);
-          break;
-      }         
-      pause(150);
+    simpleterm_close();
+
+    fdserial *ser = fdserial_open(31, 30, 0, 115200);
+
+    int val;
+
+    char c;
+
+    while (1) {
+        c = fdserial_rxCheck(ser);
+        if (c != -1) {
+
+            switch (c) {
+                case 'D' ://Drive
+                    val = fdserial_rxCheck(ser);
+                    high(26);
+                    pause(10000);
+                    low(26);
+                    break;
+                case 'G' ://Gear
+                    val = fdserial_rxCheck(ser);
+                    high(26);
+                    pause(10000);
+                    low(26);
+                    break;
+                case 'P' ://Throttle/Power
+                    val = fdserial_rxCheck(ser);
+                    high(26);
+                    pause(10000);
+                    low(26);
+                    break;
+                case 'T' ://Direction/Turn
+                    val = fdserial_rxCheck(ser);
+                    high(26);
+                    pause(10000);
+                    low(26);
+                    break;
+            }
+
+            pause(150);
+        }
     }
-  }    
 }
