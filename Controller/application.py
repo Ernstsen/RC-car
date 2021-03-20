02dcb5f080ea4f03d01deeb47c6649c5ae5d0073
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2021-03-20 20:24:09 +0100

    improved how configs are made available

--- Controller/application.py
+++ Controller/application.py
@@ -1,4 +1,5 @@
-from typing import Dict, List, Tuple
+import argparse
+from typing import Dict, List, Tuple, Callable
 
 from Controller.gui.application_gui import GUI
 from Controller.gui.model import MiscControlSpec
@@ -6,10 +7,20 @@ from Controller.vehicle_control import VehicleController
 from Controller.video import VideoStreamReceiver
 
 
+def default_vehicle_config(controller: VehicleController) -> Tuple[Dict[str, bool], List[MiscControlSpec]]:
+    """
+    Fallback configuration. Everything is disabled
+
+    :param controller: the controller used in communicating with the vehicle
+    :return: configuration for std vehicle, empty misc map
+    """
+    return {}, []
+
+
 def mini_car_config(controller: VehicleController) -> Tuple[Dict[str, bool], List[MiscControlSpec]]:
     """
     Generates and outputs configuration map
-
+    :param controller: the controller used in communicating with the vehicle
     :return: configuration for MiniCar, Misc map
     """
     enabled = {
@@ -27,12 +38,26 @@ def mini_car_config(controller: VehicleController) -> Tuple[Dict[str, bool], Lis
 
 
 if __name__ == "__main__":
+    configuration_builders: Dict[str, Callable[[VehicleController], Tuple[Dict[str, bool], List[MiscControlSpec]]]] = {
+        "Fallback": default_vehicle_config,
+        "MiniCar": mini_car_config
+    }
+
     vehicle_controller: VehicleController = VehicleController("127.0.0.1", 8080)
 
-    mini_enabled, mini_misc = mini_car_config(vehicle_controller)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-config", dest="config", default="Fallback", type=str)
 
-    streamer = VideoStreamReceiver(8000)
-    gui = GUI(viewer=streamer, controller=vehicle_controller, enabled=mini_enabled, misc_controls=mini_misc)
+    args = parser.parse_args()
 
-    vehicle_controller.start_stream()
-    gui.mainloop()
+    if args.config in configuration_builders:
+        enabled, misc = configuration_builders[args.config](vehicle_controller)
+
+        streamer = VideoStreamReceiver(8000)
+        gui = GUI(viewer=streamer, controller=vehicle_controller, enabled=enabled, misc_controls=misc)
+
+        vehicle_controller.start_stream()
+        gui.mainloop()
+    else:
+        print("Configuration '" + args.config + "' not found. Options are: " + ", ".join(
+            list(configuration_builders.keys())))
--- README.md
+++ README.md
@@ -8,11 +8,16 @@ laptop on the same network as the car
 The project consists of multiple devices, working together. This section will describe the devices and the connection.
 The devices are
 
-- [Raspberry Pi 3 Model B+](https://www.raspberrypi.org/products/raspberry-pi-3-model-b/)
+- [Raspberry Pi Zero](https://thepihut.com/collections/raspberry-pi-kits-and-bundles/products/raspberry-pi-zero-essential-kit)
   with [camera](https://www.raspberrypi.org/products/camera-module-v2/). This will host the interface to the car, and
   supply a video feed from its front.
 - [Propeller Activity Board WX](https://www.parallax.com/product/propeller-activity-board-wx/). This octa-core
   microprocessor will be the heart of the car. It will control the DC-motor, lights etc.
+
+## Vehicles
+
+Different vehicle configurations and setups can be created using the repository. As they are created and tested they
+will be found in the [wiki](https://github.com/Ernstsen/RC-car/wiki).
 
 # Getting started
 
