79dd606a304bc19fd6f93851b4d93529ed83deb7
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2023-06-04 15:06:11 +0200

    introduced logging of stream-related commands to be attempted

--- Controller/vehicle_control/vehicle_controller.py
+++ Controller/vehicle_control/vehicle_controller.py
@@ -35,22 +35,27 @@ class VehicleController(VehicleControllerI):
         """
         Requests stream initialization
         """
-        server.send(self.connection, "STREAM-INITIALIZE;" + Configurator.get_local_ip() + ";8000")
+        local_ip = Configurator.get_local_ip()
+        print("VehicleController:", "Requesting stream initialization on ip", local_ip)
+        server.send(self.connection, "STREAM-INITIALIZE;" + local_ip + ";8000")
 
     def stream_start(self) -> None:
         """
         Requests the vehicle starts streaming
         """
+        print("VehicleController:", "Requesting stream to start serving footage")
         server.send(self.connection, "STREAM-SERVE-FOOTAGE;")
 
     def stream_stop(self) -> None:
         """
         Requests that the stream stops - but not terminated
         """
+        print("VehicleController:", "Requesting stream to stop serving footage")
         server.send(self.connection, "STREAM-STOP-STREAMING;")
 
     def stream_terminate(self) -> None:
         """
         Requests that the video stream is terminated
         """
+        print("VehicleController:", "Requesting stream to terminated")
         server.send(self.connection, "STREAM-TERMINATE;")
